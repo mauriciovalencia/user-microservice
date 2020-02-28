cc0bed096c5ce68f331174483cab9c6f72e49383
Mauricio Valencia <mauricio.valencia@bitjoker.io>
2020-02-28 17:25:31 -0300

    clean code

--- src/main/java/com/challenge/user/controllers/UserRestController.java
+++ src/main/java/com/challenge/user/controllers/UserRestController.java
@@ -8,21 +8,17 @@ import com.challenge.user.models.User;
 import com.challenge.user.models.requests.UserRequest;
 import com.challenge.user.models.responses.UserResponse;
 import com.challenge.user.services.UserService;
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
+import static com.challenge.user.utils.PasswordUtils.setJWTToken;
+import static com.challenge.user.utils.PasswordUtils.setTokenableString;
+
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -37,38 +33,6 @@ public class UserRestController {
         this.userService = userService;
     }
 
-    @PostMapping("login")
-    public User login(@RequestParam("user") String username, @RequestParam("password") String pwd) {
-        // the password "pwd" can be performed for search on db register.
-        String token = getJWTToken(username);
-        User user = new User();
-        user.setUser(username);
-        user.setToken(token);
-        return user;
-    }
-
-    private String getJWTToken(String username) {
-        String secretKey = "mySecretKey"; //This secret key can be set on secret rotated, maybe AWS-KMS or something.
-        /* List<GrantedAuthority> grantedAuthorities = AuthorityUtils
-                .commaSeparatedStringToAuthorityList("ROLE_USER");
-
-        String token = Jwts
-                .builder()
-                .setId("softtekJWT")
-                .setSubject(username)
-                .claim("authorities",
-                        grantedAuthorities.stream()
-                                .map(GrantedAuthority::getAuthority)
-                                .collect(Collectors.toList()))
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 600000))
-                .signWith(SignatureAlgorithm.HS512,
-                        secretKey.getBytes()).compact(); */
-
-        //Can be performed store token before on data-base only reading by custom key of user and password.
-        // return "Bearer " + token;
-        return null;
-    }
 
     @PostMapping("users")
     public ResponseEntity<?> create (@RequestBody UserRequest request) {
@@ -81,7 +45,7 @@ public class UserRestController {
             userResponse.setId(user.getId());
             userResponse.setLastLogin(user.getTimestamps().getCreatedAt());
             userResponse.setName(user.getName());
-            userResponse.setToken("7jAqdUPWuM66XYqBjq0K");
+            userResponse.setToken(setJWTToken(setTokenableString(user.getEmail())));
             userResponse.setIsActive(user.getIsActive());
             userResponse.setTimestamps(user.getTimestamps());
             return new ResponseEntity<>(userResponse, HttpStatus.OK);
